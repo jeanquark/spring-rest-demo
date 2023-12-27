6f2111af26f97a572739a07f1c3e92dbaaf62f59
Jean-Marc <jm.kleger@gmail.com>
2023-12-28 00:21:37 +0100

    Initial commit

---
+++ src/main/java/com/webtoucan/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,38 @@
+package com.webtoucan.restdemo.controller;
+
+import com.webtoucan.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        /*return new CloudVendor("C1", "Vendor 1", "Address One", "xxxxx");*/
+
+        return cloudVendor;
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor updated successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId)
+    {
+        this.cloudVendor = null;
+        return "Cloud Vendor deleted successfully";
+    }
+}
